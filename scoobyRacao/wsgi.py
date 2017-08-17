0641b39b67314df9daf79f7a1031041e218137d5
Diego Delmiro <di3g0d0ming05@gmail.com>
2017-08-16 21:27:29 -0300

    comite inicial

---
+++ scoobyRacao/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for scoobyRacao project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "scoobyRacao.settings")
+
+application = get_wsgi_application()
