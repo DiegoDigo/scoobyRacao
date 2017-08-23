e8dec78f088b47dc13fc5d44d4e33c5e2c79cd33
diego.delmiro <di3g0d0ming05@gmail.com>
2017-08-23 17:02:08 -0300

    ajuste admin

--- produto/admin.py
+++ produto/admin.py
@@ -4,6 +4,9 @@ from .models import Produto, Categoria, Animal
 
 class AdminProduto(admin.ModelAdmin):
     list_display = ['nome', 'preco', 'categoria', 'animal']
+    search_fields = ['nome', 'categoria__tipo', 'animal__tipo']
+    list_filter = ['nome', 'categoria', 'animal']
+    save_on_top = True
 
 admin.site.register(Produto, AdminProduto)
 admin.site.register(Categoria)
